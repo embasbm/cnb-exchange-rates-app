eca6a015026f15a592cf16bb9adea8451c862b38
Emba Moussa <embasbm@gmail.com>
2023-12-02 15:39:41 +0100

    feat: make all updates happening in model currency broadcast to currency template via action cable

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -2,4 +2,6 @@ class Currency < ApplicationRecord
   validates :currency_code, uniqueness: { case_sensitive: false }
 
   scope :valid_for, -> (day) { Currency.where(valid_for: day.beginning_of_day..day.end_of_day) }
+
+  broadcasts_to ->(currency) { "currencies" }, inserts_by: :prepend
 end

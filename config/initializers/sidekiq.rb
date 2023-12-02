becd8620b8691980494b867be5c01c84d1fd0a37
Emba Moussa <embasbm@gmail.com>
2023-12-02 20:55:59 +0100

    feat: schedule the job for 6AM everyday, except weekends

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,8 @@ require 'sidekiq'
 require 'sidekiq-cron'
 
 Sidekiq::Cron::Job.create(
-  name: 'CnbApi::FetcherWorker',
-  cron: '0 6 * * *', # Run daily at 6 AM, TODO: discuss this
-  class: 'CnbApi::FetcherWorker'
+  name: 'Daily CNB API exchange rates fetcher worker',
+  cron: '0 6 * * 1-5', # The cron job is scheduled to run every day (Monday to Friday) at 6 AM.
+  class: 'CnbApi::FetcherWorker',
+  args: {path: '/cnbapi/exrates/daily', parameters: {lang: 'EN'}}
 )

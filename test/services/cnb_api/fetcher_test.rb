80a8eeeb62c8b62dba6b137e4e5380d86b4112cd
Emba Moussa <embasbm@gmail.com>
2023-12-03 15:44:47 +0100

    specs: fix specs accordingly to new params on worker perform method

--- test/services/cnb_api/fetcher_test.rb
+++ test/services/cnb_api/fetcher_test.rb
@@ -6,7 +6,10 @@ class CnbApi::FetcherTest < ActiveSupport::TestCase
 
   test "fetch_data returns DailyDump ID when daily dump exists" do
     daily_dump = daily_dumps(:one)
-    fetcher = CnbApi::Fetcher.new('/some_path', date: daily_dump.file_name.to_date)
+    fetcher = CnbApi::Fetcher.new(
+      path: '/some_path',
+      parameters: { date: daily_dump.file_name.to_date, lang: 'EN' }
+    )
 
     assert_equal daily_dump.id, fetcher.fetch_data
   end
@@ -18,7 +21,10 @@ class CnbApi::FetcherTest < ActiveSupport::TestCase
     mock_response.expect(:body, response_body)
 
     Net::HTTP.stub(:get_response, mock_response) do
-      fetcher = CnbApi::Fetcher.new('/some_path', date: Date.parse('2022-11-29'))
+      fetcher = CnbApi::Fetcher.new(
+        path: '/some_path',
+        parameters: { date: Date.parse('2022-11-29'), lang: 'EN' }
+      )
 
       assert_difference 'DailyDump.count', 1 do
         assert_kind_of Integer, fetcher.fetch_data
@@ -33,7 +39,10 @@ class CnbApi::FetcherTest < ActiveSupport::TestCase
     mock_response.expect(:is_a?, true, [Object])
 
     Net::HTTP.stub(:get_response, mock_response) do
-      fetcher = CnbApi::Fetcher.new('/some_path', date: Date.parse('2022-11-29'))
+      fetcher = CnbApi::Fetcher.new(
+        path: '/some_path',
+        parameters: { date: Date.parse('2022-11-29'), lang: 'EN' }
+      )
 
       assert_nil fetcher.fetch_data
     end
